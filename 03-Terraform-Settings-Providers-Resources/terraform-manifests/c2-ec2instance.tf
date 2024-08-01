f6110b5d0930da78e4f45f8420add2ff83fb9196
ukhare <ukhare@outlook.com>
2024-08-01 18:32:41 +0530

    create basic ec2 instance

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,7 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  #ami = "ami-0742b4e673072066f"
+  ami = "ami-0427090fd1714168b"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
